958ca17d867bf5301860658181278203cb6d4475
sonvice <wvmc_es@hotmail.com>
2023-09-20 11:23:44 +0200

    tag meta social

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -13,7 +13,7 @@ const CardProject = ({ nunCard,titleMain }) => {
       <h2 className="dark:text-slate-300 text-size-2"># {titleMain}</h2>
       <div className="box-grid gap-lg mt-lg">
         {newArr.map((figure) => (
-          <div key={figure.label} className="card">
+          <div key={figure.label} className="card pb-xs">
             <figure>
               <a href={figure.href} target="_blank">
                 <img
@@ -25,7 +25,7 @@ const CardProject = ({ nunCard,titleMain }) => {
               <h3 className="text-base mt-sm dark:text-slate-300">{figure.title}</h3>
             <Figcaption {...figure} otroValor={'Valor'}></Figcaption>
             </figure>
-            <div>
+            <div className="mt-xs">
             <a className="btn-link dark:text-slate-400 dark:decoration-slate-400" href={figure.href} target="_blank">{figure.title} <RiShareBoxLine className="align-middle"></RiShareBoxLine></a>
                 <a href={figure.gitHub} target="_blank"><RiGithubFill className="align-middle ml-sm dark:text-slate-400 dark:decoration-slate-400"></RiGithubFill></a>
             </div>
