fc0c93b96669f823983fdbcea0958a7b6a8c7f96
sonvice <wvmc_es@hotmail.com>
2023-12-23 21:51:51 +0100

    Sitemap add astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
   site: 'https://wilsonmunoz.com/',
   markdown: {
     shikiConfig: {
