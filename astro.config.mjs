6c06d85e81363e4f1b1b24aea724a1289a90079c
sonvice <wvmc_es@hotmail.com>
2024-01-03 20:30:11 +0100

    Add google analytics + upgrade Astro v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx(), sitemap()],
-  site: 'https://wilsonmunoz.com/',
+  integrations: [
+    react(),
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+  site: "https://wilsonmunoz.com/",
   markdown: {
     shikiConfig: {
-      theme: 'monokai',
-      wrap: true
-    }
-  }
+      theme: "monokai",
+      wrap: true,
+    },
+  },
 });
