77e17fecab4abfb6918d0bbcc898e48efff9103a
sonvice <wvmc_es@hotmail.com>
2024-02-25 19:58:56 +0100

    Add new post - Mis apuntes

---
+++ src/content/posts/images/mis-apuntes-del-curso-de-ux/index.js
@@ -0,0 +1,19 @@
+import personas from './personas-ux.jpg';
+import historyUser from './historia-de-usuario.jpg';
+import userMap from './mapa-de-recorrido-de-usuario.png';
+import problem from './planteamiento-del-problema.png';
+import esquema from './esquema-de-pagina-crazy-eights.jpg';
+import prototipoBajo from './prototipo-baja-fidelidad.png';
+import mapaSitio from './mapa-de-sitio.jpg';
+import maquetaAlta from './maqueta-de-alta-fidelidad.png';
+
+export{
+    personas,
+    historyUser,
+    userMap,
+    problem,
+    esquema,
+    prototipoBajo,
+    mapaSitio,
+    maquetaAlta
+}
