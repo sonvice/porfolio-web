ae1fdf9e3d4f8247b0f5036bd9467e928f6bff3e
sonvice <wvmc_es@hotmail.com>
2024-09-15 21:09:20 +0200

    add Adapter netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,25 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import partytown from '@astrojs/partytown';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  output:'hybrid',
-  integrations: [
-    react(),
-    mdx(),
-    sitemap(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  output: 'hybrid',
+  integrations: [react(), mdx(), sitemap(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  })],
   site: "https://wilsonmunoz.com/",
   markdown: {
     shikiConfig: {
       theme: "monokai",
-      wrap: true,
-    },
+      wrap: true
+    }
   },
+  adapter: netlify()
 });
