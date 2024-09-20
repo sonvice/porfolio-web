93120b82dc727bcd836707764ccbf51ec8535a1e
sonvice <wvmc_es@hotmail.com>
2024-09-20 22:51:08 +0200

    fix:(package.json)

--- .netlify/v1/functions/ssr/.netlify/build/chunks/2023-12-14-guia-efectiva-de-diseno-web-como-centrar-en-css_DTOhMid3.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./2023-12-14-guia-efectiva-de-diseno-web-como-centrar-en-css_UKyJIrhj.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
