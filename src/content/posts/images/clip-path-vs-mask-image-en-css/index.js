d8d998b3eb41c5daa624540177a24c96baf5aded
sonvice <wvmc_es@hotmail.com>
2025-01-08 14:43:15 +0100

    Add new post clip-path vs mask

---
+++ src/content/posts/images/clip-path-vs-mask-image-en-css/index.js
@@ -0,0 +1,12 @@
+import figmaSVG from './figma-svg-export-2025-01-08.png';
+import maskImg from './mask-img-2025-01-08.png';
+import svgFooter from './svg-footer-2025-01-08.png';
+import landingPortada from './landig-portada.png';
+
+
+export{
+    figmaSVG,
+    maskImg,
+    svgFooter,
+    landingPortada
+}
