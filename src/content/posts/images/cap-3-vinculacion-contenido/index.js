4dd5de759b9c8975c054089998397071ee429492
sonvice <wvmc_es@hotmail.com>
2025-11-16 14:44:13 +0100

    Add Capítulo 3: Vinculación de Contenido (Hyperlinks)

---
+++ src/content/posts/images/cap-3-vinculacion-contenido/index.js
@@ -0,0 +1,9 @@
+import fileName from './file-accessible.png';
+import retiro from './parque-el-retiro-madrid.webp';
+import herdphone from './heardphone.webp';
+
+export{
+    fileName,
+    retiro,
+    herdphone
+}
