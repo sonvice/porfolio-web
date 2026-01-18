54595d4ccf6fda91513479d7c0faece8e98d6c90
sonvice <wvmc_es@hotmail.com>
2026-01-18 19:35:15 +0100

    Add new post cap. 5

---
+++ src/content/posts/images/cap-5-estilizando-contenido/index.js
@@ -0,0 +1,11 @@
+import colorContrasteImg  from './cap-5-color-contraste.webp';
+import preferenciasImg from './cap-5-preferencias-usuario.webp';
+import unidadesImg from './cap-5-unidades-relativas.webp';
+import animacionesImg from './cap-5-animaciones-seguras.webp';
+
+export {
+    colorContrasteImg,
+    preferenciasImg,
+    unidadesImg,
+    animacionesImg
+}
